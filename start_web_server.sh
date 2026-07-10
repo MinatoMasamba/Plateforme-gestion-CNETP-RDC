fd7e12e399452907fb2e1b7487d86cbfd651b97d
MinatoMasamba <edmasamba100@gmail.com>
2026-07-10 19:37:16 +0200

    ok

--- start_web_server.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Lance un simple serveur HTTP Python pour servir les fichiers statiques.
-# Nécessite Python 3.
-
-# Assurez-vous d'être dans le bon répertoire pour servir les fichiers
-cd frontend_static
-
-echo "Démarrage du serveur HTTP sur http://localhost:8000/"
-python3 -m http.server 8000
